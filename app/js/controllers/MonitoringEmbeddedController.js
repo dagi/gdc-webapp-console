39ec9fcf8136cb5f93471f98c50c39c88461759d
Roman Pichlik <dagi@gooddata.com>
2013-10-08 16:02:26 +0200

    Set Hystrix redirect URL regarding to the environment

--- app/js/controllers/MonitoringEmbeddedController.js
+++ app/js/controllers/MonitoringEmbeddedController.js
@@ -2,6 +2,10 @@
     App.MonitoringEmbeddedController = Ember.Controller.extend({
 
         iframeUrl : function() {
+            if(window.location.hostname == "localhost" && window.location.port == 8080) {
+                //this is intended for Java developers running stand-alone Tomcat with management-console
+                return "hystrix-dashboard/monitor/monitor.html?stream=http%3A%2F%2Flocalhost%3A8080%2Fturbine.stream%3Fcluster%3DWebappHystrixCluster";
+            }
             return "hystrix-dashboard/monitor/monitor.html?stream=http%3A%2F%2Flocalhost%3A8080%2Fmanagement-console%2Fturbine.stream%3Fcluster%3DWebappHystrixCluster";
         }.property()
     });
